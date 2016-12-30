57e81a457c8761804566b694f4417a11da90dfb6
Brian Marx <b.marx981@gmail.com>
2016-12-29 19:33:38 -0500

    Initial Commit. Nothing works yet.

--- Waveform/ViewController.swift
+++ Waveform/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
 
+    var osc: AKOscillator?
+    let wave = AKTable(.sawtooth, count: 4096)
+    var filter: AKLowPassFilter?
+    var mixer: AKMixer?
+    var oscFreq = 2000.0
+    var filterFreq = 2000.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        osc = AKOscillator(waveform: wave, frequency: oscFreq)
+        filter = AKLowPassFilter(osc!, cutoffFrequency: filterFreq)
+        mixer = AKMixer(filter!)
+        mixer?.start()
+        
+        AudioKit.output = mixer
+        AudioKit.start()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func OscFreqSlider(_ sender: UISlider) {
+    }
+    
+    @IBAction func FilterFreqSlider(_ sender: UISlider) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
